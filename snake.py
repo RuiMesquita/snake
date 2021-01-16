4a271ddee95fae14390b2ff76a07a29f6e7a9229
Rui Mesquita <ruipedrorochamesquita@gmail.com>
2021-01-16 13:37:58 +0000

    Paused menu/Score/capture food added

--- snake.py
+++ snake.py
@@ -48,13 +48,16 @@ class Food(object):
         self.x, self.y = self.randomize_position()
 
     def randomize_position(self):
-        self.x = random.randint(0, GRID_WIDTH) * GRID_SIZE
-        self.y = random.randint(0, GRID_HEIGHT) * GRID_SIZE
+        self.x = random.randint(0, GRID_WIDTH - 1) * GRID_SIZE
+        self.y = random.randint(0, GRID_HEIGHT - 1) * GRID_SIZE
         return (self.x, self.y)
     
     def draw(self):
         shape = pg.Rect((int(self.x), int(self.y)), (GRID_SIZE, GRID_SIZE))
         pg.draw.rect(window, self.color, shape)
+    
+    def get_position(self):
+        return (self.x, self.y)
 
 
 def draw_grid(surface):
@@ -69,15 +72,52 @@ def draw_grid(surface):
             else:
                 square = pg.Rect((x*GRID_SIZE, y*GRID_SIZE), (GRID_SIZE, GRID_SIZE))
                 pg.draw.rect(surface, (218, 247, 166), square)
-            
 
 
+def text_objects(text, font):
+    """
+    Print text to a surface
+    """
+    text_surface = font.render(text, True, (0, 0, 0))
+    return text_surface, text_surface.get_rect() 
+
+
+def pause_game():
+    """
+    Introduces an infinite loop in the game loop in order to pause tha game
+    """
+    large_text = pg.font.SysFont("Arial", 20)
+    MENU_WIDTH, MENU_HEIGHT = 400, 200
+    shape = (SCREEN_WIDTH / 2 - (MENU_WIDTH / 2), SCREEN_HEIGHT / 2 - (MENU_HEIGHT / 2), MENU_WIDTH, MENU_HEIGHT)
+
+    pg.draw.rect(window, (255, 255, 255), shape, 0)
+    pg.draw.rect(window, (0, 0, 0), shape, 2)
+
+    text_surf, text_rect = text_objects("Press ESC to resume", large_text)
+    text_rect.center = ((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    window.blit(text_surf, text_rect)
+
+    pg.display.update()
+
+    paused = True
+    while paused:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                quit() 
+
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    paused = False
 
 snake = Snake()
 food = Food()
 
 pg.init()
 pg.display.set_caption("Snake Game")
+
+font = pg.font.Font("freesansbold.ttf", 17)
+
 clock = pg.time.Clock()
 running = True
 window = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -87,6 +127,10 @@ while running:
     clock.tick(10)
     draw_grid(window)
     pg.display.flip()
+
+    if snake.get_head_position() == food.get_position():
+        food.randomize_position()
+        snake.score += 1
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -101,9 +145,13 @@ while running:
                 snake.move(LEFT)
             elif event.key == pg.K_RIGHT:
                 snake.move(RIGHT)
+            elif event.key == pg.K_ESCAPE:
+                pause_game()
         
     snake.draw()
     food.draw()
+    text = font.render(f"Score: {snake.score}", 1, (0, 0, 0))
+    window.blit(text, (5, 10))
     pg.display.update()
 
 pg.quit()
