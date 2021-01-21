a380dcc2ec8fa64b52ddf8b122ee5a629c0efde4
Rui Mesquita <ruipedrorochamesquita@gmail.com>
2021-01-21 13:38:54 +0000

    Labels added to the buttons

--- main_menu.py
+++ main_menu.py
@@ -4,35 +4,42 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 class Button(object):
-    def __init__(self, position):
+    def __init__(self, position, label):
         self.position = position
         self.color = (255, 195, 0)
         self.width, self.height = 300, 50
+        self.label = label
     
-    def draw(self):
+    def draw(self, font):
         rect = pygame.Rect(self.position, (self.width, self.height))
-        pygame.draw.rect(screen, self.color, rect)
+        pygame.draw.rect(screen, self.color, rect, 0)
+        pygame.draw.rect(screen, (0, 0, 0), rect, 2)
+
+        text_surface = font.render(self.label, True, (255, 255, 255))
+        text_rect = text_surface.get_rect()
+        text_rect.center = (SCREEN_WIDTH / 2, self.position[1] + self.height/2)
+        screen.blit(text_surface, text_rect)
         
     def blink(self):
         pass
-    
 
-button_sp = Button((SCREEN_WIDTH/2 - 150, 200))
-button_mp = Button((SCREEN_WIDTH/2 - 150, 300))
-button_exit = Button((SCREEN_WIDTH/2 - 150, 400))
+button_sp = Button((SCREEN_WIDTH/2 - 150, 200), "SinglePlayer")
+button_mp = Button((SCREEN_WIDTH/2 - 150, 300), "MultiPlayer")
+button_exit = Button((SCREEN_WIDTH/2 - 150, 400), "Exit")
 
 pygame.init()
 
 title_font = pygame.font.Font("freesansbold.ttf", 60)
+label_font = pygame.font.Font("freesansbold.ttf", 20)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 screen.fill((218, 247, 166))
 pygame.display.set_caption("Main Menu")
 
 running = True
 while running:
-    button_sp.draw()
-    button_mp.draw()
-    button_exit.draw()
+    button_sp.draw(label_font)
+    button_mp.draw(label_font)
+    button_exit.draw(label_font)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
